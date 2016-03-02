390eab14883b9837416e14e1603a6b8e99420c6d
xiaolonw <xiaolonw@cs.cmu.edu>
2016-03-01 20:49:57 -0500

    scratch

--- render_scripts/scratch_cls/solver_p.py
+++ render_scripts/scratch_cls/solver_p.py
@@ -16,18 +16,8 @@ caffe.set_device(2)
 solver = caffe.SGDSolver('solver.prototxt')
 # solver.net.copy_from('/scratch/16824/models/bvlc_reference_caffenet.caffemodel')
 
-niter = 100000
 train_loss = np.zeros(niter)
 
-f = open('log.txt', 'w')
-
-for it in range(niter): 
-    solver.step(1)
-    train_loss[it] = solver.net.blobs['loss'].data
-    f.write('{0: f}\n'.format(train_loss[it]))
-
-f.close()
-
-# solver.step(80000)
+solver.step(100000)
 
 
